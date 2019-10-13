e062366d149fa215653bb17cd13ffc9fae40b844
Kizito Njoku <nkizito57@gmail.com>
2019-10-13 03:34:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,8 +2,8 @@ def mario
   phrase = "'It's-a me, Mario!'"
   puts phrase
 end
-$status
+
 def toadstool
-  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
